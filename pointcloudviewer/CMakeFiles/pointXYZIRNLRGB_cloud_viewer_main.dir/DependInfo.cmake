f5d0fa5dc4058f4972ced03401c24ac92740ceea
nickcharron <nicholas.c.charron@gmail.com>
2017-10-31 13:48:39 -0400

    Adding point cloud viewer

---
+++ pointcloudviewer/CMakeFiles/pointXYZIRNLRGB_cloud_viewer_main.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ncharron/pcl/pointcloudviewer/src/data_model.cpp" "/home/ncharron/pcl/pointcloudviewer/CMakeFiles/pointXYZIRNLRGB_cloud_viewer_main.dir/src/data_model.cpp.o"
+  "/home/ncharron/pcl/pointcloudviewer/src/pointXYZIRNLRGB_cloud_viewer_main.cpp" "/home/ncharron/pcl/pointcloudviewer/CMakeFiles/pointXYZIRNLRGB_cloud_viewer_main.dir/src/pointXYZIRNLRGB_cloud_viewer_main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "qh_QHpointer"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/vtk-5.8"
+  "include"
+  "/usr/include/pcl-1.7"
+  "/usr/include/eigen3"
+  "/usr/include/ni"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
