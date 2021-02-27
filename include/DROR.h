5ce8ffdfdc7b59e1c33be7a38cfce6a33120949f
a.kuzembayev@sergek.kz <5pY8G1PN11#IWxJcd3QNz>
2021-02-27 17:34:22 +0600

    templated dror

--- include/DROR.h
+++ include/DROR.h
@@ -10,74 +10,111 @@
  */
 class DROR {
 public:
-  /**
-   * @brief constructor
-   */
-  DROR();
+    /**
+     * @brief constructor
+     */
+    DROR();
 
-  /**
-   * @brief destructor
-   */
-  ~DROR();
+    /**
+     * @brief destructor
+     */
+    ~DROR();
 
-  /**
-   * @brief Method for setting radius multiplier
-   * @param radius_multiplier
-   */
-  void SetRadiusMultiplier(double radius_multiplier);
+    /**
+     * @brief Method for setting radius multiplier
+     * @param radius_multiplier
+     */
+    void SetRadiusMultiplier(double radius_multiplier);
 
-  /**
-   * @brief Method for retrieving radius multiplier
-   * @return radius_multiplier default = 3. This should be greater than 1
-   */
-  double GetRadiusMultiplier();
+    /**
+     * @brief Method for retrieving radius multiplier
+     * @return radius_multiplier default = 3. This should be greater than 1
+     */
+    double GetRadiusMultiplier();
 
-  /**
-   * @brief Method for setting azimuth angle
-   * @param azimuth_angle default = 0.04 degees
-   */
-  void SetAzimuthAngle(double azimuth_angle);
+    /**
+     * @brief Method for setting azimuth angle
+     * @param azimuth_angle default = 0.04 degees
+     */
+    void SetAzimuthAngle(double azimuth_angle);
 
-  /**
-  * @brief Method for getting azimuth angle in degrees
-  * @return azimuth_angle
-  */
-  double GetAzimuthAngle();
+    /**
+    * @brief Method for getting azimuth angle in degrees
+    * @return azimuth_angle
+    */
+    double GetAzimuthAngle();
 
-  /**
-   * @brief Method for setting minimum number of neighbors which includes the
-   * point that is being filtered (3 means 2 neighbors)
-   * @param min_neighbors default = 3
-   */
-  void SetMinNeighbors(double min_neighbors);
+    /**
+     * @brief Method for setting minimum number of neighbors which includes the
+     * point that is being filtered (3 means 2 neighbors)
+     * @param min_neighbors default = 3
+     */
+    void SetMinNeighbors(double min_neighbors);
 
-  /**
-   * @brief Method for retrieving minimum number of neighbors
-   * @return min_neighbors
-   */
-  double GetMinNeighbors();
+    /**
+     * @brief Method for retrieving minimum number of neighbors
+     * @return min_neighbors
+     */
+    double GetMinNeighbors();
 
-  /**
-   * @brief Method for setting the minimum search radius
-   * @param min_search_radius default = 0.04
-   */
-  void SetMinSearchRadius(double min_search_radius);
+    /**
+     * @brief Method for setting the minimum search radius
+     * @param min_search_radius default = 0.04
+     */
+    void SetMinSearchRadius(double min_search_radius);
 
-  /**
-   * @brief Method for retrieving the minimum search radius
-   * @return min_search_radius
-   */
-  double GetMinSearchRadius();
+    /**
+     * @brief Method for retrieving the minimum search radius
+     * @return min_search_radius
+     */
+    double GetMinSearchRadius();
 
-  /**
-   * @brief Method for applying the filter
-   * @param input_cloud cloud to be filtered
-   * @param filtered_cloud cloud to save to
-   */
-  void Filter(pcl::PointCloud<pcl::PointXYZI>::Ptr& input_cloud,
-              pcl::PointCloud<pcl::PointXYZI>& filtered_cloud);
+    /**
+     * @brief Method for applying the filter
+     * @param input_cloud cloud to be filtered
+     * @param filtered_cloud cloud to save to
+     */
+
+    template<typename T>
+    void Filter(typename pcl::PointCloud<T>::Ptr &input_cloud, typename pcl::PointCloud<T> &filtered_cloud);
 
 private:
-  double radius_multiplier_, azimuth_angle_, min_neighbors_,
-         min_search_radius_;
+    double radius_multiplier_, azimuth_angle_, min_neighbors_,
+            min_search_radius_;
 };
+
+template<typename T>
+void DROR::Filter(typename pcl::PointCloud<T>::Ptr &input_cloud, typename pcl::PointCloud<T> &filtered_cloud) {
+    using KdTreePtr = typename pcl::KdTreeFLANN<T>::Ptr;
+//    Clear points in output cloud
+    filtered_cloud.clear();
+
+    // init. kd search tree
+    KdTreePtr kd_tree_(new pcl::KdTreeFLANN<T>());
+    kd_tree_->setInputCloud(input_cloud);
+
+    // Go over all the points and check which doesn't have enough neighbors
+    // perform filtering
+    for (typename pcl::PointCloud<T>::iterator it = input_cloud->begin();
+         it != input_cloud->end(); ++it) {
+        float x_i = it->x;
+        float y_i = it->y;
+        float range_i = sqrt(pow(x_i, 2) + pow(y_i, 2));
+        float search_radius_dynamic =
+                radius_multiplier_ * azimuth_angle_ * M_PI / 180 * pow(range_i / 5, 3);
+
+        if (search_radius_dynamic < min_search_radius_) {
+            search_radius_dynamic = min_search_radius_;
+        }
+
+        std::vector<int> pointIdxRadiusSearch;
+        std::vector<float> pointRadiusSquaredDistance;
+
+        int neighbors = kd_tree_->radiusSearch(*it, search_radius_dynamic, pointIdxRadiusSearch,
+                                               pointRadiusSquaredDistance);
+
+        if (neighbors >= min_neighbors_) {
+            filtered_cloud.push_back(*it);
+        }
+    }
+}
--- src/DROR.cpp
+++ src/DROR.cpp
@@ -1,83 +1,44 @@
 #include "DROR.h"
 
-typedef pcl::KdTreeFLANN<pcl::PointXYZI> KdTree;
-typedef KdTree::Ptr KdTreePtr;
-typedef pcl::PointCloud<pcl::PointXYZI> PointCloud;
-typedef PointCloud::Ptr PointCloudPtr;
-
-DROR::DROR(){
-  radius_multiplier_=3;
-  azimuth_angle_=0.04;
-  min_neighbors_=3;
-  min_search_radius_=0.04;
+DROR::DROR() {
+    radius_multiplier_ = 3;
+    azimuth_angle_ = 0.04;
+    min_neighbors_ = 3;
+    min_search_radius_ = 0.04;
 }
 
-DROR::~DROR(){
-  //
+DROR::~DROR() {
+    //
 }
 
 void DROR::SetRadiusMultiplier(double radius_multiplier) {
-  radius_multiplier_ = radius_multiplier;
+    radius_multiplier_ = radius_multiplier;
 }
 
 double DROR::GetRadiusMultiplier() {
-  return radius_multiplier_;
+    return radius_multiplier_;
 }
 
 void DROR::SetAzimuthAngle(double azimuth_angle) {
-  azimuth_angle_ = azimuth_angle;
+    azimuth_angle_ = azimuth_angle;
 }
 
 double DROR::GetAzimuthAngle() {
-  return azimuth_angle_;
+    return azimuth_angle_;
 }
 
 void DROR::SetMinNeighbors(double min_neighbors) {
-  min_neighbors_ = min_neighbors;
+    min_neighbors_ = min_neighbors;
 }
 
 double DROR::GetMinNeighbors() {
-  return min_neighbors_;
+    return min_neighbors_;
 }
 
 void DROR::SetMinSearchRadius(double min_search_radius) {
-  min_search_radius_ = min_search_radius;
+    min_search_radius_ = min_search_radius;
 }
 
 double DROR::GetMinSearchRadius() {
-  return min_search_radius_;
-}
-
-void DROR::Filter(pcl::PointCloud<pcl::PointXYZI>::Ptr& input_cloud,
-                  pcl::PointCloud<pcl::PointXYZI>& filtered_cloud) {
-  // Clear points in output cloud
-  filtered_cloud.clear();
-
-  // init. kd search tree
-  KdTreePtr kd_tree_(new pcl::KdTreeFLANN<pcl::PointXYZI>());
-  kd_tree_->setInputCloud(input_cloud);
-
-  // Go over all the points and check which doesn't have enough neighbors
-  // perform filtering
-  for (pcl::PointCloud<pcl::PointXYZI>::iterator it = input_cloud->begin();
-       it != input_cloud->end(); ++it) {
-    float x_i = it->x;
-    float y_i = it->y;
-    float range_i = sqrt(pow(x_i, 2) + pow(y_i, 2));
-    float search_radius_dynamic =
-        radius_multiplier_ * azimuth_angle_ * 3.14159265359 / 180 * range_i;
-
-    if (search_radius_dynamic < min_search_radius_) {
-      search_radius_dynamic = min_search_radius_;
-    }
-
-    std::vector<int> pointIdxRadiusSearch;
-    std::vector<float> pointRadiusSquaredDistance;
-
-    int neighbors =
-        kd_tree_->radiusSearch(*it, search_radius_dynamic, pointIdxRadiusSearch,
-                               pointRadiusSquaredDistance);
-
-    if (neighbors >= min_neighbors_) { filtered_cloud.push_back(*it); }
-  }
+    return min_search_radius_;
 }
